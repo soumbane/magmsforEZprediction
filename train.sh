f4db135bf5b0f1b6b0b331acdce72fe71e7740ed
soumbane <s.banerjee@wayne.edu>
2023-06-15 15:50:05 -0400

    minor changes

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python train.py /home/share/Data/EZ_Pred_Dataset/All_Hemispheres/ /home/neil/Lab_work/Jeong_Lab_Multi_Modal_MRI/magmsEZpred/trained_models/test_948.model -node 948 -b 64 -lr 5e-5 -e 100 --seed 100 -exp test_948.exp --replace_experiment --show_verbose --device cuda:1
+python train.py /home/share/Data/EZ_Pred_Dataset/All_Hemispheres/ /home/neil/Lab_work/Jeong_Lab_Multi_Modal_MRI/magmsEZpred/trained_models/test_948.model -node 948 -b 16 -lr 1e-2 -e 20 --seed 518 -exp test_948.exp --replace_experiment --show_verbose --device cuda:1
