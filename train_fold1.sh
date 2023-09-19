d1fff678cfb4b8939fa335f97033d447306c471c
soumbane <s.banerjee@wayne.edu>
2023-09-19 12:42:10 -0400

    delete unnecessary files

--- train_fold1.sh
+++
@@ -1,8 +0,0 @@
-python train.py /home/user1/Desktop/Soumyanil_EZ_Pred_project/Data/All_Hemispheres/SMOTE_Augmented_Data/ /home/user1/Desktop/Soumyanil_EZ_Pred_project/Models/magmsforEZprediction/trained_models/magms_T1_DWIC_WB_fold1.model -b 32 -lr 5e-4 --num_mod 5 --fold_no 1 --train_mod T1-DWIC -e 25 -exp magms_T1_DWIC_WB_fold1.exp --replace_experiment --show_verbose --device cuda:0
-
-python train.py /home/user1/Desktop/Soumyanil_EZ_Pred_project/Data/All_Hemispheres/SMOTE_Augmented_Data/ /home/user1/Desktop/Soumyanil_EZ_Pred_project/Models/magmsforEZprediction/trained_models/magms_T1_T2_DWIC_WB_fold1.model -b 32 -lr 5e-4 --num_mod 5 --fold_no 1 --train_mod T1-T2-DWIC -e 25 -exp magms_T1_T2_DWIC_WB_fold1.exp --replace_experiment --show_verbose --device cuda:0
-
-python train.py /home/user1/Desktop/Soumyanil_EZ_Pred_project/Data/All_Hemispheres/SMOTE_Augmented_Data/ /home/user1/Desktop/Soumyanil_EZ_Pred_project/Models/magmsforEZprediction/trained_models/magms_T1_T2_DWI_DWIC_WB_fold1.model -b 32 -lr 5e-4 --num_mod 5 --fold_no 1 --train_mod T1-T2-DWI-DWIC -e 25 -exp magms_T1_T2_DWI_DWIC_WB_fold1.exp --replace_experiment --show_verbose --device cuda:0
-
-
-python train.py /home/user1/Desktop/Soumyanil_EZ_Pred_project/Data/All_Hemispheres/SMOTE_Augmented_Data/ /home/user1/Desktop/Soumyanil_EZ_Pred_project/Models/magmsforEZprediction/trained_models/magms_T1_DWIC_WB_fold5.model -b 32 -lr 5e-4 --num_mod 5 --fold_no 5 --train_mod T1-DWIC -e 25 -exp magms_T1_DWIC_WB_fold5.exp --replace_experiment --show_verbose --device cuda:0
