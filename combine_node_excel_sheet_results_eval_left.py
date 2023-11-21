b263c235e91731cb9f39b89f390ff3c325d85068
soumbane <s.banerjee@wayne.edu>
2023-11-21 15:05:38 -0500

    updated ALL modality comb aggregation file

--- combine_node_excel_sheet_results_eval_left.py
+++ combine_node_excel_sheet_results_eval_left.py
@@ -2,16 +2,18 @@ import pandas as pd
 import os
 
 # Define the paths of your Excel files
-# base_path = '/home/neil/Lab_work/Jeong_Lab_Multi_Modal_MRI/Left_Temporal_Lobe/'
-base_path = '/media/user1/MyHDataStor41/Soumyanil_EZ_Pred_project/Data/All_Hemispheres/Left_Temporal_Lobe/'
+# base_path = '/home/neil/Lab_work/Jeong_Lab_Multi_Modal_MRI/Left_Temporal_Lobe/Part_2/'
+base_path = '/media/user1/MyHDataStor41/Soumyanil_EZ_Pred_project/Data/All_Hemispheres/Left_Temporal_Lobe/Part_2/'
 
-node_nums = ["385","386","387","388","389","390","391","392","393","394","395","396","397","398","399","400","401","402","403","404","405","406","407","408","409","410","411","412","413","414","415","416","417","418","419","420","421","422","423","424","425","426","427","428","429","430","431","432","433","434","435","436","437","438","439","440","441","442","443","444","445","446","447","448","449","450","451","452","453","454","455","456","458","459","460","461","462","463","464","465","466","467","468","469","470","471","472","473","474","475","476","477","478","479"]
+# node_nums = ["385","386","387","388","389","390","391","392","393","394","395","396","397","398","399","400","401","402","403","404","405","406","407","408","409","410","411","412","413","414","415","416","417","418","419","420","421","422","423","424","425","426","427","428","429","430","431","432","433","434","435","436","437","438","439","440","441","442","443","444","445","446","447","448","449","450","451","452","453","454","455","456","458","459","460","461","462","463","464","465","466","467","468","469","470","471","472","473","474","475","476","477","478","479"] # part 1
+
+node_nums = ["386","387","388","389","390","391","394","395","396","397","398","399","400","401","402","403","404","405","406","408","409","410","411","412","413","414","415","416","417","418","419","420","421","422","423","424","426","427","428","429","430","431","432","433","435","436","437","438","439","440","441","442","443","444","445","446","447","448","450","451","452","453","454","455","456","458","459","460","461","462","463","464","465","466","467","469","470","471","472","473","474","475","476","477","478","479"] # part 2
 
 file_paths_val = []
 
 for node_num in node_nums:
-    # file_path_val = os.path.join(base_path, "Node_"+node_num+"_Results", "Eval_Results", "results_val_ALL_modalities.xlsx") # For ALL modality combinations
-    file_path_val = os.path.join(base_path, "Node_"+node_num+"_Results", "Eval_Results", "results_LeftTemp_val_FULL_Modality_Only.xlsx") # For FULL modality Only
+    file_path_val = os.path.join(base_path, "Node_"+node_num+"_Results", "Eval_Results", "results_val_ALL_modalities_Part_2.xlsx") # For ALL modality combinations
+    # file_path_val = os.path.join(base_path, "Node_"+node_num+"_Results", "Eval_Results", "results_LeftTemp_val_FULL_Modality_Only.xlsx") # For FULL modality Only
     file_paths_val.append(file_path_val)
 
 # Initialize an empty DataFrame
@@ -23,12 +25,13 @@ for path in file_paths_val:
     df = pd.read_excel(path)  
 
     # Stack the rows
-    # combined_df_val = pd.concat([combined_df_val, df], axis=1) # For ALL modality combinations
-    combined_df_val = pd.concat([combined_df_val, df], axis=0) # For FULL modality Only
+    combined_df_val = pd.concat([combined_df_val, df], axis=1) # For ALL modality combinations
+    # combined_df_val = pd.concat([combined_df_val, df], axis=0) # For FULL modality Only
 
 # Reset the index to avoid duplicate row indices
 combined_df_val = combined_df_val.reset_index(drop=True)
 
 # Save the combined DataFrame to a new Excel file
-combined_df_val.to_excel('LeftTemp_val_FULL_modality_Only.xlsx', index=False)
+# combined_df_val.to_excel('LeftTemp_val_FULL_modality_Only.xlsx', index=False)
+combined_df_val.to_excel('LeftTemp_val_ALL_modality_Comb_Part_2.xlsx', index=False)
 
--- combine_node_excel_sheet_results_eval_right.py
+++ combine_node_excel_sheet_results_eval_right.py
@@ -2,22 +2,30 @@ import pandas as pd
 import os
 
 # Define the paths of your Excel files
-# base_path = '/home/neil/Lab_work/Jeong_Lab_Multi_Modal_MRI/Right_Temporal_Lobe/'
-base_path = '/media/user1/MyHDataStor41/Soumyanil_EZ_Pred_project/Data/All_Hemispheres/Right_Temporal_Lobe/'
+# base_path = '/home/neil/Lab_work/Jeong_Lab_Multi_Modal_MRI/Right_Temporal_Lobe/Part_2/'
+base_path = '/media/user1/MyHDataStor41/Soumyanil_EZ_Pred_project/Data/All_Hemispheres/Right_Temporal_Lobe/Part_2/'
+
+# node_nums = ["888","889","890","891","892","893","894","895",
+#     "896","897","898","899","900","901","902","903","904","905","906","907","908","909","910","911","912",
+#     "913","914","915","916","917","918","919","920","921","922","923","924","925","926","927","928","929",
+#     "930","931","932","933","934","935","936","937","938","939","940","941","942","943","944","945","946",
+#     "947","948","949","950","951","952","953","954","955","956","957","958","959","960","961",
+#     "962","963","964","965","966","968","969","970","971","973","974","975","976","977","978","979",
+#     "980","981","982","983"] # part 1
 
 node_nums = ["888","889","890","891","892","893","894","895",
     "896","897","898","899","900","901","902","903","904","905","906","907","908","909","910","911","912",
     "913","914","915","916","917","918","919","920","921","922","923","924","925","926","927","928","929",
-    "930","931","932","933","934","935","936","937","938","939","940","941","942","943","944","945","946",
-    "947","948","949","950","951","952","953","954","955","956","957","958","959","960","961",
-    "962","963","964","965","966","968","969","970","971","973","974","975","976","977","978","979",
-    "980","981","982","983"]
+    "930","931","932","933","934","935","937","938","939","940","941","942","943","944","945","946",
+    "947","948","949","950","951","952","953","954","955","956","957","958","960","961",
+    "962","963","964","965","968","969","970","971","973","974","975","976","977","978","979",
+    "980","981","982","983"] # part 2
 
 file_paths_val = []
 
 for node_num in node_nums:
-    # file_path_val = os.path.join(base_path, "Node_"+node_num+"_Results", "Eval_Results", "results_val_ALL_modalities.xlsx") # For ALL modality combinations
-    file_path_val = os.path.join(base_path, "Node_"+node_num+"_Results", "Eval_Results", "results_RightTemp_val_FULL_Modality_Only.xlsx") # For FULL modality Only
+    file_path_val = os.path.join(base_path, "Node_"+node_num+"_Results", "Eval_Results", "results_val_ALL_modalities_Part_2.xlsx") # For ALL modality combinations
+    # file_path_val = os.path.join(base_path, "Node_"+node_num+"_Results", "Eval_Results", "results_RightTemp_val_FULL_Modality_Only.xlsx") # For FULL modality Only
     file_paths_val.append(file_path_val)
 
 # Initialize an empty DataFrame
@@ -29,12 +37,13 @@ for path in file_paths_val:
     df = pd.read_excel(path)  
 
     # Stack the rows
-    # combined_df_val = pd.concat([combined_df_val, df], axis=1) # For ALL modality combinations
-    combined_df_val = pd.concat([combined_df_val, df], axis=0) # For FULL modality Only
+    combined_df_val = pd.concat([combined_df_val, df], axis=1) # For ALL modality combinations
+    # combined_df_val = pd.concat([combined_df_val, df], axis=0) # For FULL modality Only
 
 # Reset the index to avoid duplicate row indices
 combined_df_val = combined_df_val.reset_index(drop=True)
 
 # Save the combined DataFrame to a new Excel file
-combined_df_val.to_excel('RightTemp_val_FULL_modality_Only.xlsx', index=False)
+# combined_df_val.to_excel('RightTemp_val_FULL_modality_Only.xlsx', index=False)
+combined_df_val.to_excel('RightTemp_val_ALL_modality_Comb_Part_2.xlsx', index=False)
 
