146a9a4c4d9d9a126587f6971ab46b14844b7fbd
soumbane <s.banerjee@wayne.edu>
2023-12-10 18:26:53 -0500

    minor updates to training script

--- train_right.py
+++ train_right.py
@@ -101,154 +101,154 @@ def train(cfg: TrainingConfigs, /) -> Union[magnet.MAGNET2, Tuple[float, float, 
 
 
 if __name__ == "__main__":
-    # # get configs
-    # configs = TrainingConfigs.from_arguments()
-    # assert isinstance(configs, TrainingConfigs)
-
-    # base_exp_name = configs.experiment
-
-    # num_trials = 5
-    
-    # # Create empty lists to store results for each type (bal_accuracy, sensitivity, specificity)
-    # val_bal_acc_list = [[] for _ in range(num_trials)]
-    # val_sen_list = [[] for _ in range(num_trials)]
-    # val_spec_list = [[] for _ in range(num_trials)]
-
-    # train_bal_acc_list = [[] for _ in range(num_trials)]
-    # train_sen_list = [[] for _ in range(num_trials)]
-    # train_spec_list = [[] for _ in range(num_trials)]
-
-    # # train
-    # for i in range(num_trials):
-    #     print(f'\n\nStarting Trial {i+1} of Node number {configs.node_num}\n')
-
-    #     configs.experiment = base_exp_name + "_trial" + str(i+1) + ".exp"
-
-    #     val_bal_acc, val_sen, val_spec, train_bal_acc, train_sen, train_spec = train(configs) # type:ignore
-
-    #     val_bal_acc_list[i].append(val_bal_acc) 
-    #     val_sen_list[i].append(val_sen)         
-    #     val_spec_list[i].append(val_spec) 
-
-    #     train_bal_acc_list[i].append(train_bal_acc) 
-    #     train_sen_list[i].append(train_sen) 
-    #     train_spec_list[i].append(train_spec) 
-
-    # # Combine data
-    # row_data_val = [configs.node_num] + [val_bal_acc_list[j][0] for j in range(num_trials)] + [val_sen_list[j][0] for j in range(num_trials)] + [val_spec_list[j][0] for j in range(num_trials)] 
-
-    # row_data_train = [configs.node_num] + [train_bal_acc_list[j][0] for j in range(num_trials)] + [train_sen_list[j][0] for j in range(num_trials)] + [train_spec_list[j][0] for j in range(num_trials)]
-
-    # # Create a DataFrame
-    # headers_val = ['Node #', 'Val_Balanced_Accuracy_1', 'Val_Balanced_Accuracy_2', 'Val_Balanced_Accuracy_3', 'Val_Balanced_Accuracy_4', 'Val_Balanced_Accuracy_5', 'Val_sensitivity_1', 'Val_sensitivity_2', 'Val_sensitivity_3', 'Val_sensitivity_4', 'Val_sensitivity_5', 'Val_specificity_1', 'Val_specificity_2', 'Val_specificity_3', 'Val_specificity_4', 'Val_specificity_5']
-    
-    # headers_train = ['Node #', 'Train_Balanced_Accuracy_1', 'Train_Balanced_Accuracy_2', 'Train_Balanced_Accuracy_3', 'Train_Balanced_Accuracy_4', 'Train_Balanced_Accuracy_5', 'Train_sensitivity_1', 'Train_sensitivity_2', 'Train_sensitivity_3', 'Train_sensitivity_4', 'Train_sensitivity_5', 'Train_specificity_1', 'Train_specificity_2', 'Train_specificity_3', 'Train_specificity_4', 'Train_specificity_5']
-
-    # df_val = pd.DataFrame([row_data_val], columns=headers_val)
-
-    # df_train = pd.DataFrame([row_data_train], columns=headers_train)
-
-    # # Saving to Excel
-    # # path = "/home/neil/Lab_work/Jeong_Lab_Multi_Modal_MRI/Right_Temporal_Lobe/"  
-    # # path = "/media/user1/MyHDataStor41/Soumyanil_EZ_Pred_project/Data/All_Hemispheres/Right_Temporal_Lobe/Part_2/"
-    # path = "/media/user1/MyHDataStor41/Soumyanil_EZ_Pred_project/Data/All_Hemispheres/Right_Hemis/Part_2/"
-    # save_path = os.path.join(path, "Node_"+str(configs.node_num), "Results")
-
-    # if not os.path.exists(save_path):
-    #     os.makedirs(save_path)
-
-    # filename_val = "results_val.xlsx"
-    # # filename_val = "results_val_NO_Distillation.xlsx"
-    # save_filepath_val = os.path.join(save_path, filename_val)
-
-    # df_val.to_excel(save_filepath_val, index=False, sheet_name='Sheet1')
-
-    # filename_train = "results_train.xlsx"
-    # # filename_train = "results_train_NO_Distillation.xlsx"
-    # save_filepath_train = os.path.join(save_path, filename_train)
-
-    # df_train.to_excel(save_filepath_train, index=False, sheet_name='Sheet1')
-
-    # print("\nDone!")
-
-    ###############################################################################
-    # get configs - perform only 1 or 2 trials to modify the experiments for trial 
+    # get configs
     configs = TrainingConfigs.from_arguments()
     assert isinstance(configs, TrainingConfigs)
 
     base_exp_name = configs.experiment
 
-    num_trials = [configs.trial_num] # the trial you want to try minus 1 (e.g. if you want to try trial 1, then put 0 here)
-    if num_trials[0] + 1 == 1:
-        select_data_bal_acc = 'B2'
-        select_data_sen = 'G2'
-        select_data_spec = 'L2'
-    elif num_trials[0] + 1 == 2:
-        select_data_bal_acc = 'C2'
-        select_data_sen = 'H2'
-        select_data_spec = 'M2'
-    elif num_trials[0] + 1 == 3:
-        select_data_bal_acc = 'D2'
-        select_data_sen = 'I2'
-        select_data_spec = 'N2'
-    elif num_trials[0] + 1 == 4:
-        select_data_bal_acc = 'E2'
-        select_data_sen = 'J2'
-        select_data_spec = 'O2'
-    elif num_trials[0] + 1 == 5:
-        select_data_bal_acc = 'F2'
-        select_data_sen = 'K2'
-        select_data_spec = 'P2'
-    else:
-        raise ValueError('The trial number is not valid!')
+    num_trials = 5
     
-    # Load the excel files - both for validation and training
-    path = "/media/user1/MyHDataStor41/Soumyanil_EZ_Pred_project/Data/All_Hemispheres/Right_Hemis/Part_2/"
-    load_path = os.path.join(path, "Node_"+str(configs.node_num), "Results")
+    # Create empty lists to store results for each type (bal_accuracy, sensitivity, specificity)
+    val_bal_acc_list = [[] for _ in range(num_trials)]
+    val_sen_list = [[] for _ in range(num_trials)]
+    val_spec_list = [[] for _ in range(num_trials)]
 
-    # Load validation file
-    filename_val = "results_val.xlsx"
-    load_filepath_val = os.path.join(load_path, filename_val)
-
-    val_file = openpyxl.load_workbook(load_filepath_val)
-    val_sheet = val_file['Sheet1']
-
-    # Load Training file
-    filename_train = "results_train.xlsx"
-    load_filepath_train = os.path.join(load_path, filename_train)
-
-    train_file = openpyxl.load_workbook(load_filepath_train)
-    train_sheet = train_file['Sheet1']
+    train_bal_acc_list = [[] for _ in range(num_trials)]
+    train_sen_list = [[] for _ in range(num_trials)]
+    train_spec_list = [[] for _ in range(num_trials)]
 
     # train
-    for i in num_trials:
+    for i in range(num_trials):
         print(f'\n\nStarting Trial {i+1} of Node number {configs.node_num}\n')
 
         configs.experiment = base_exp_name + "_trial" + str(i+1) + ".exp"
 
         val_bal_acc, val_sen, val_spec, train_bal_acc, train_sen, train_spec = train(configs) # type:ignore
 
-        val_sheet[select_data_bal_acc] = val_bal_acc
-        val_sheet[select_data_sen] = val_sen
-        val_sheet[select_data_spec] = val_spec
+        val_bal_acc_list[i].append(val_bal_acc) 
+        val_sen_list[i].append(val_sen)         
+        val_spec_list[i].append(val_spec) 
 
-        train_sheet[select_data_bal_acc] = train_bal_acc
-        train_sheet[select_data_sen] = train_sen
-        train_sheet[select_data_spec] = train_spec
+        train_bal_acc_list[i].append(train_bal_acc) 
+        train_sen_list[i].append(train_sen) 
+        train_spec_list[i].append(train_spec) 
+
+    # Combine data
+    row_data_val = [configs.node_num] + [val_bal_acc_list[j][0] for j in range(num_trials)] + [val_sen_list[j][0] for j in range(num_trials)] + [val_spec_list[j][0] for j in range(num_trials)] 
+
+    row_data_train = [configs.node_num] + [train_bal_acc_list[j][0] for j in range(num_trials)] + [train_sen_list[j][0] for j in range(num_trials)] + [train_spec_list[j][0] for j in range(num_trials)]
+
+    # Create a DataFrame
+    headers_val = ['Node #', 'Val_Balanced_Accuracy_1', 'Val_Balanced_Accuracy_2', 'Val_Balanced_Accuracy_3', 'Val_Balanced_Accuracy_4', 'Val_Balanced_Accuracy_5', 'Val_sensitivity_1', 'Val_sensitivity_2', 'Val_sensitivity_3', 'Val_sensitivity_4', 'Val_sensitivity_5', 'Val_specificity_1', 'Val_specificity_2', 'Val_specificity_3', 'Val_specificity_4', 'Val_specificity_5']
     
+    headers_train = ['Node #', 'Train_Balanced_Accuracy_1', 'Train_Balanced_Accuracy_2', 'Train_Balanced_Accuracy_3', 'Train_Balanced_Accuracy_4', 'Train_Balanced_Accuracy_5', 'Train_sensitivity_1', 'Train_sensitivity_2', 'Train_sensitivity_3', 'Train_sensitivity_4', 'Train_sensitivity_5', 'Train_specificity_1', 'Train_specificity_2', 'Train_specificity_3', 'Train_specificity_4', 'Train_specificity_5']
+
+    df_val = pd.DataFrame([row_data_val], columns=headers_val)
+
+    df_train = pd.DataFrame([row_data_train], columns=headers_train)
+
+    # Saving to Excel
+    # path = "/home/neil/Lab_work/Jeong_Lab_Multi_Modal_MRI/Right_Temporal_Lobe/"  
+    # path = "/media/user1/MyHDataStor41/Soumyanil_EZ_Pred_project/Data/All_Hemispheres/Right_Temporal_Lobe/Part_2/"
+    path = "/media/user1/MyHDataStor41/Soumyanil_EZ_Pred_project/Data/All_Hemispheres/Right_Hemis/Part_2/"
     save_path = os.path.join(path, "Node_"+str(configs.node_num), "Results")
-    
-    # Save modified validation file
-    filename_val = "results_val.xlsx"
-    save_filepath_val = os.path.join(save_path, filename_val)
-    val_file.save(save_filepath_val)
-    val_file.close()
 
-    # Save modified training file
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    filename_val = "results_val.xlsx"
+    # filename_val = "results_val_NO_Distillation.xlsx"
+    save_filepath_val = os.path.join(save_path, filename_val)
+
+    df_val.to_excel(save_filepath_val, index=False, sheet_name='Sheet1')
+
     filename_train = "results_train.xlsx"
+    # filename_train = "results_train_NO_Distillation.xlsx"
     save_filepath_train = os.path.join(save_path, filename_train)
-    train_file.save(save_filepath_train)
-    train_file.close()
+
+    df_train.to_excel(save_filepath_train, index=False, sheet_name='Sheet1')
 
     print("\nDone!")
+
+    ###############################################################################
+    # # get configs - perform only 1 or 2 trials to modify the experiments for trial 
+    # configs = TrainingConfigs.from_arguments()
+    # assert isinstance(configs, TrainingConfigs)
+
+    # base_exp_name = configs.experiment
+
+    # num_trials = [configs.trial_num] # the trial you want to try minus 1 (e.g. if you want to try trial 1, then put 0 here)
+    # if num_trials[0] + 1 == 1:
+    #     select_data_bal_acc = 'B2'
+    #     select_data_sen = 'G2'
+    #     select_data_spec = 'L2'
+    # elif num_trials[0] + 1 == 2:
+    #     select_data_bal_acc = 'C2'
+    #     select_data_sen = 'H2'
+    #     select_data_spec = 'M2'
+    # elif num_trials[0] + 1 == 3:
+    #     select_data_bal_acc = 'D2'
+    #     select_data_sen = 'I2'
+    #     select_data_spec = 'N2'
+    # elif num_trials[0] + 1 == 4:
+    #     select_data_bal_acc = 'E2'
+    #     select_data_sen = 'J2'
+    #     select_data_spec = 'O2'
+    # elif num_trials[0] + 1 == 5:
+    #     select_data_bal_acc = 'F2'
+    #     select_data_sen = 'K2'
+    #     select_data_spec = 'P2'
+    # else:
+    #     raise ValueError('The trial number is not valid!')
+    
+    # # Load the excel files - both for validation and training
+    # path = "/media/user1/MyHDataStor41/Soumyanil_EZ_Pred_project/Data/All_Hemispheres/Right_Hemis/Part_2/"
+    # load_path = os.path.join(path, "Node_"+str(configs.node_num), "Results")
+
+    # # Load validation file
+    # filename_val = "results_val.xlsx"
+    # load_filepath_val = os.path.join(load_path, filename_val)
+
+    # val_file = openpyxl.load_workbook(load_filepath_val)
+    # val_sheet = val_file['Sheet1']
+
+    # # Load Training file
+    # filename_train = "results_train.xlsx"
+    # load_filepath_train = os.path.join(load_path, filename_train)
+
+    # train_file = openpyxl.load_workbook(load_filepath_train)
+    # train_sheet = train_file['Sheet1']
+
+    # # train
+    # for i in num_trials:
+    #     print(f'\n\nStarting Trial {i+1} of Node number {configs.node_num}\n')
+
+    #     configs.experiment = base_exp_name + "_trial" + str(i+1) + ".exp"
+
+    #     val_bal_acc, val_sen, val_spec, train_bal_acc, train_sen, train_spec = train(configs) # type:ignore
+
+    #     val_sheet[select_data_bal_acc] = val_bal_acc  # type:ignore
+    #     val_sheet[select_data_sen] = val_sen  # type:ignore
+    #     val_sheet[select_data_spec] = val_spec  # type:ignore
+
+    #     train_sheet[select_data_bal_acc] = train_bal_acc  # type:ignore
+    #     train_sheet[select_data_sen] = train_sen  # type:ignore
+    #     train_sheet[select_data_spec] = train_spec  # type:ignore
+    
+    # save_path = os.path.join(path, "Node_"+str(configs.node_num), "Results")
+    
+    # # Save modified validation file
+    # filename_val = "results_val.xlsx"
+    # save_filepath_val = os.path.join(save_path, filename_val)
+    # val_file.save(save_filepath_val)
+    # val_file.close()
+
+    # # Save modified training file
+    # filename_train = "results_train.xlsx"
+    # save_filepath_train = os.path.join(save_path, filename_train)
+    # train_file.save(save_filepath_train)
+    # train_file.close()
+
+    # print("\nDone!")
 
